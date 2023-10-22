73946f3d14034a52e41d1b4a226a553f192cfbab
alex-aelenei <alex.aelenei@aelsoftware.com>
2023-10-22 10:15:57 +0300

    Initial commit

---
+++ src/main/java/tmbe/TmBeApplication.java
@@ -0,0 +1,13 @@
+package tmbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tmbe/TmBeApplicationTests.java
@@ -0,0 +1,13 @@
+package tmbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
